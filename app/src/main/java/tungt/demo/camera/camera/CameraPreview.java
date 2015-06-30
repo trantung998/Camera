57daecfba9304b510a90f7b87c57b22282b16e9c
trantung998 <melodyinmyheart91@gmail.com>
2015-06-30 23:04:46 +0700

    backup code

--- app/src/main/java/tungt/demo/camera/camera/CameraPreview.java
+++ app/src/main/java/tungt/demo/camera/camera/CameraPreview.java
@@ -1,20 +1,25 @@
 package tungt.demo.camera.camera;
 
 import android.app.Activity;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.hardware.Camera;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 6/14/2015.
  */
-public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder mHolder;
     private Camera mCamera;
     private Activity mActivity;
@@ -30,7 +35,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     }
 
-    void initHolder(){
+    void initHolder() {
         this.mHolder = getHolder();
         this.mHolder.addCallback(this);
         mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
@@ -43,17 +48,17 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        Log.d("surfaceCreated","");
+        Log.d("surfaceCreated", "");
         Log.d("mCamera", "" + mCamera);
         Log.d("holder", "" + holder);
 
         try {
-        if(mCamera != null){
+            if (mCamera != null) {
 
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        }
-        }catch (IOException e){
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            }
+        } catch (IOException e) {
             Log.d(VIEW_LOG_TAG, "Error setting camera preview: " + e.getMessage());
         }
     }
@@ -82,7 +87,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         // set preview size and make any resize, rotate or
         // reformatting changes here
         // start preview with new settings
-        mCamera= camera;
+        mCamera = camera;
         configureCameraParameters();
         try {
             mCamera.setPreviewDisplay(mHolder);
@@ -96,7 +101,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         int angle;
         int degrees = 0;
         int rotation = mActivity.getWindowManager().getDefaultDisplay().getRotation();
-        Log.d("rotation" , rotation+ "");
+        Log.d("rotation", rotation + "");
         switch (rotation) {
             case Surface.ROTATION_0: // This is display orientation
                 angle = 90; // This is camera orientation
@@ -121,24 +126,21 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
 
         Camera.Parameters params = mCamera.getParameters();
         Camera.CameraInfo info = new Camera.CameraInfo();
-        int cameraside = (isFront)?Camera.CameraInfo.CAMERA_FACING_FRONT:Camera.CameraInfo.CAMERA_FACING_BACK;
+        int cameraside = (isFront) ? Camera.CameraInfo.CAMERA_FACING_FRONT : Camera.CameraInfo.CAMERA_FACING_BACK;
         Camera.getCameraInfo(cameraside, info);
         Log.d("info.orientation", info.orientation + "");
         Log.d("info.facing", info.facing + "");
         int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT)
-        {
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
 //            result = (info.orientation + degrees) % 360;
 //            result = (360 - result) % 360; // compensate the mirror
             params.setRotation(degrees);
-        }
-        else
-        { // back-facing
+        } else { // back-facing
 //            result = (info.orientation - degrees + 360) % 360;
             params.setRotation(angle);
         }
-        mCamera.setDisplayOrientation(angle);
-        mCamera.setParameters(params);
+        mCamera.setDisplayOrientation(angle); //Set preview orientation of camera preview folow the screen orientation
+        mCamera.setParameters(params);        //Set new parameter for camera
     }
 
     public boolean isFront() {
@@ -148,87 +150,95 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     public void setIsFront(boolean isFront) {
         this.isFront = isFront;
     }
-//===================//
+
+    //===================//
     private boolean listenerSet = false;
     public Paint paint;
     private DrawingView drawingView;
     private boolean drawingViewSet = false;
 
-//    Camera.AutoFocusCallback myAutoFocusCallback = new Camera.AutoFocusCallback(){
-//
-//        @Override
-//        public void onAutoFocus(boolean arg0, Camera arg1) {
-//            if (arg0){
-//                mCamera.cancelAutoFocus();
-//            }
-//        }
-//    };
+    Camera.AutoFocusCallback myAutoFocusCallback = new Camera.AutoFocusCallback() {
 
-//
+        @Override
+        public void onAutoFocus(boolean arg0, Camera arg1) {
+            if (arg0) {
+                mCamera.cancelAutoFocus();
+            }
+        }
+    };
+
+    //
 //    /**
 //     * Called from PreviewSurfaceView to set touch focus.
 //     * @param - Rect - new area for auto focus
 //     */
-//    public void doTouchFocus(final Rect tfocusRect) {
-//        try {
-//            List<Camera.Area> focusList = new ArrayList<Camera.Area>();
-//            Camera.Area focusArea = new Camera.Area(tfocusRect, 1000);
-//            focusList.add(focusArea);
-//
-//            Camera.Parameters param = mCamera.getParameters();
-//            param.setFocusAreas(focusList);
-//            param.setMeteringAreas(focusList);
-//            mCamera.setParameters(param);
-//
-//            mCamera.autoFocus(myAutoFocusCallback);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            Log.i("Camera", "Unable to autofocus");
-//        }
-//    }
-//
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        Log.d("Touch","onTouchEvent");
+    public void doTouchFocus(final Rect tfocusRect) {
+        try {
+            List<Camera.Area> focusList = new ArrayList<Camera.Area>();
+            Camera.Area focusArea = new Camera.Area(tfocusRect, 1000);
+            focusList.add(focusArea);
+
+            Camera.Parameters param = mCamera.getParameters();
+            param.setFocusAreas(focusList);
+            param.setMeteringAreas(focusList);
+            mCamera.setParameters(param);
+            mCamera.autoFocus(myAutoFocusCallback);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.i("Camera", "Unable to autofocus");
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("Touch", "onTouchEvent");
 //        if (!listenerSet) {
 //            return false;
 //        }
-//        if(event.getAction() == MotionEvent.ACTION_DOWN){
-//            float x = event.getX();
-//            float y = event.getY();
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            float x = event.getX();
+            float y = event.getY();
+            if (mHolder.getSurface().isValid()) {
+                Canvas canvas = mHolder.lockCanvas();
+                canvas.drawColor(Color.BLACK);
+                canvas.drawCircle(event.getX(), event.getY(), 50, paint);
+                mHolder.unlockCanvasAndPost(canvas);
 //
-//            Rect touchRect = new Rect(
-//                    (int)(x - 100),
-//                    (int)(y - 100),
-//                    (int)(x + 100),
-//                    (int)(y + 100));
+//                Rect touchRect = new Rect(
+//                        (int) (x - 100),
+//                        (int) (y - 100),
+//                        (int) (x + 100),
+//                        (int) (y + 100));
 //
-//            final Rect targetFocusRect = new Rect(
-//                    touchRect.left * 2000/this.getWidth() - 1000,
-//                    touchRect.top * 2000/this.getHeight() - 1000,
-//                    touchRect.right * 2000/this.getWidth() - 1000,
-//                    touchRect.bottom * 2000/this.getHeight() - 1000);
+//                final Rect targetFocusRect = new Rect(
+//                        touchRect.left * 2000 / this.getWidth() - 1000,
+//                        touchRect.top * 2000 / this.getHeight() - 1000,
+//                        touchRect.right * 2000 / this.getWidth() - 1000,
+//                        touchRect.bottom * 2000 / this.getHeight() - 1000);
 //
-//            doTouchFocus(targetFocusRect);
-//            if (drawingViewSet) {
-//                drawingView.setHaveTouch(true, touchRect);
-//                drawingView.invalidate();
+//                doTouchFocus(targetFocusRect);
+//                if (drawingViewSet) {
+//                    drawingView.setHaveTouch(true, touchRect);
+//                    drawingView.invalidate();
 //
-//                // Remove the square after some time
-//                Handler handler = new Handler();
-//                handler.postDelayed(new Runnable() {
+//                    // Remove the square after some time
+//                    Handler handler = new Handler();
+//                    handler.postDelayed(new Runnable() {
 //
-//                    @Override
-//                    public void run() {
-//                        drawingView.setHaveTouch(false, new Rect(0, 0, 0, 0));
-//                        drawingView.invalidate();
-//                    }
-//                }, 1000);
-//            }
-//
-//        }
-//        return false;
-//    }
+//                        @Override
+//                        public void run() {
+//                            drawingView.setHaveTouch(false, new Rect(0, 0, 0, 0));
+//                            drawingView.invalidate();
+//                        }
+//                    }, 1000);
+//                }
+
+            }
+            return true;
+        }
+        return false;
+    }
+
 
     public void setDrawingView(DrawingView dView) {
         drawingView = dView;
--- app/src/main/java/tungt/demo/camera/camera/DrawingView.java
+++ app/src/main/java/tungt/demo/camera/camera/DrawingView.java
@@ -2,16 +2,18 @@ package tungt.demo.camera.camera;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 
 /**
  * Created by 9i-tungt on 6/18/2015.
  */
-public class DrawingView extends View {
+public class DrawingView extends SurfaceView {
     private boolean haveTouch = false;
     private Rect touchArea;
     private Paint paint;
@@ -38,5 +40,21 @@ public class DrawingView extends View {
                     touchArea.left, touchArea.top, touchArea.right, touchArea.bottom,
                     paint);
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("Touch", "onTouchEvent");
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+            if (getHolder().getSurface().isValid()) {
+                Canvas canvas = getHolder().lockCanvas();
+                canvas.drawColor(Color.BLACK);
+                canvas.drawCircle(event.getX(), event.getY(), 50, paint);
+                getHolder().unlockCanvasAndPost(canvas);
+            }
+            return true;
+        }
+        return false;
     }
 }
